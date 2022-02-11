8ba55af74c0b14802e2f5c2efaced9435fe8d186
孔令皓 <konglh@superred.com.cn>
2022-02-11 17:22:32 +0800

    完成利用两个栈转换成一个队列方法联系

--- src/main/java/com/fouo/tx/day03/Code06_TwoStacksImplementQueue.java
+++ src/main/java/com/fouo/tx/day03/Code06_TwoStacksImplementQueue.java
@@ -4,45 +4,96 @@ import java.util.Stack;
 
 public class Code06_TwoStacksImplementQueue {
 
+	/**
+	 * PUSH    POP
+	 *         1
+	 *         2
+	 *         3
+	 * 7       4
+	 * 6       5
+	 *
+	 * PUSH栈倒数据要一次性倒完全
+	 *
+	 * POP栈没有拿完，PUSH不能倒数据
+	 */
 	public static class TwoStacksQueue {
 		public Stack<Integer> stackPush;
 		public Stack<Integer> stackPop;
 
 		public TwoStacksQueue() {
-			stackPush = new Stack<Integer>();
-			stackPop = new Stack<Integer>();
+			this.stackPop = new Stack<Integer>();
+			this.stackPush = new Stack<Integer>();
 		}
 
-		// push栈向pop栈倒入数据
-		private void pushToPop() {
-			if (stackPop.empty()) {
-				while (!stackPush.empty()) {
-					stackPop.push(stackPush.pop());
+		//倒数据
+		public void pushToPop(){
+			if(this.stackPop.isEmpty()){
+				while(!this.stackPush.isEmpty()){
+					this.stackPop.push(this.stackPush.pop());
 				}
 			}
 		}
-
+		//添加数据（向组成的队列中）
 		public void add(int pushInt) {
-			stackPush.push(pushInt);
 			pushToPop();
+			this.stackPush.push(pushInt);
 		}
 
 		public int poll() {
-			if (stackPop.empty() && stackPush.empty()) {
-				throw new RuntimeException("Queue is empty!");
-			}
 			pushToPop();
-			return stackPop.pop();
+			if(this.stackPop.isEmpty()){
+				throw new RuntimeException("your queue is empty");
+			}
+			return this.stackPop.pop();
+		}
+		public int peek() {
+			pushToPop();
+			if(this.stackPop.isEmpty()){
+				throw new RuntimeException("your queue is empty");
+			}
+			return this.stackPop.peek();
 		}
 
-		public int peek() {
-			if (stackPop.empty() && stackPush.empty()) {
-				throw new RuntimeException("Queue is empty!");
-			}
-			pushToPop();
-			return stackPop.peek();
-		}
 	}
+//	public static class TwoStacksQueue {
+//		public Stack<Integer> stackPush;
+//		public Stack<Integer> stackPop;
+//
+//		public TwoStacksQueue() {
+//			stackPush = new Stack<Integer>();
+//			stackPop = new Stack<Integer>();
+//		}
+//
+//		// push栈向pop栈倒入数据
+//		private void pushToPop() {
+//			if (stackPop.empty()) {
+//				while (!stackPush.empty()) {
+//					stackPop.push(stackPush.pop());
+//				}
+//			}
+//		}
+//
+//		public void add(int pushInt) {
+//			stackPush.push(pushInt);
+//			pushToPop();
+//		}
+//
+//		public int poll() {
+//			if (stackPop.empty() && stackPush.empty()) {
+//				throw new RuntimeException("Queue is empty!");
+//			}
+//			pushToPop();
+//			return stackPop.pop();
+//		}
+//
+//		public int peek() {
+//			if (stackPop.empty() && stackPush.empty()) {
+//				throw new RuntimeException("Queue is empty!");
+//			}
+//			pushToPop();
+//			return stackPop.peek();
+//		}
+//	}
 
 	public static void main(String[] args) {
 		TwoStacksQueue test = new TwoStacksQueue();
