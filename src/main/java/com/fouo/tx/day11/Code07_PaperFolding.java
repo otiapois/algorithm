77d1f100f2f0b08e662052db1135b6d8ac102764
孔令皓 <konglh@superred.com.cn>
2022-05-18 08:29:35 +0800

    二叉树递归套路

--- src/main/java/com/fouo/tx/day11/Code07_PaperFolding.java
+++ src/main/java/com/fouo/tx/day11/Code07_PaperFolding.java
@@ -1,12 +1,28 @@
 package com.fouo.tx.day11;
-
+/**
+ * @author KONGLH
+ * @title: 折纸，凹痕的左右可看作是左右子树
+ * 			左子树是凹痕
+ * 			右子树是凸痕
+ * 		    按照中序排序的方式就可以将展开的痕迹完全打印出来
+ * @projectName
+ * @description:
+ * @date  2022/5/17 17:28
+ */
 public class Code07_PaperFolding {
 
 	public static void printAllFolds(int N) {
 		process(1, N, true);
 		System.out.println();
 	}
-
+//	public static void process(int i, int N, boolean down) {
+//		if(i>N){
+//			return;
+//		}
+//		process(i+1,N,true);
+//		System.out.print(down?"凹 ":"凸 ");
+//		process(i+1,N,false);
+//	}
 	// 当前你来了一个节点，脑海中想象的！
 	// 这个节点在第i层，一共有N层，N固定不变的
 	// 这个节点如果是凹的话，down = T
@@ -20,9 +36,10 @@ public class Code07_PaperFolding {
 		System.out.print(down ? "凹 " : "凸 ");
 		process(i + 1, N, false);
 	}
-
+	//凹 凹 凸 凹 凹 凸 凸
+	//凹 凹 凸 凹 凹 凸 凸
 	public static void main(String[] args) {
-		int N = 4;
+		int N = 3;
 		printAllFolds(N);
 	}
 }
